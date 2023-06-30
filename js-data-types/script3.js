299b47c6500b95762dc3cd45daaec20c00429340
amlak <amlakbarkon@gmail.com>
2023-06-29 20:11:52 -0500

    check me

--- js-data-types/script3.js
+++ js-data-types/script3.js
@@ -1,1 +1,19 @@
 console.log("script3")
+const splitMe = "I am the eggman I am the walrus";
+
+function splitString(str) {
+  return str.split(" ");
+  
+}
+
+const newString = splitString(splitMe);
+const amlak =[]
+newString.forEach(element => {
+    if(element =='the'){
+        amlak.push('amlak')
+    }else{
+        amlak.push(element)
+    }
+});
+console.log(newString);
+console.log(amlak)
